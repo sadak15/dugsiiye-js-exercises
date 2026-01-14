e06231b7263dc366a20b84bbd07f33655fca6182
sadak15 <sadakc15@gmail.com>
2026-01-14 20:57:23 +0300

    exercise 2

---
+++ exercise2/exercise2.js
@@ -0,0 +1,13 @@
+let a = 15;
+let b = "15";
+
+
+console.log(a == b);  
+
+console.log(a === b);
+console.log(a != b); 
+console.log(a !== b); 
+console.log(a > b);  
+console.log(a < b);
+console.log(a >= b);
+console.log(a <= b);
