8e3906d5b0559a9d13d2fcb2447e0495c62bd401
sadak15 <sadakc15@gmail.com>
2026-01-21 22:37:43 +0300

    exercise 10

---
+++ exercise10/exercise10.js
@@ -0,0 +1,17 @@
+
+let library = [
+    {
+        title: "Maanso",
+        author: "Hadraawi",
+        year: 2017
+    },
+
+    {
+        title: "Garaadso",
+        author: "Mubarak Hadi",
+        year: 2023
+    }
+]
+
+console.log(library[0].title);
+console.log(library[1].author);
