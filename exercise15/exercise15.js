3fb0aae568028c9c437c14226b79bc36d05318b3
sadak15 <sadakc15@gmail.com>
2026-01-27 00:03:21 +0300

    exercise 15 compinateion array and object

--- exercise15/exercise15.js
+++ exercise15/exercise15.js
@@ -1,7 +1,22 @@
 
-
+//show object only 
 const person = { name: "Alice", age: 25, city: "Wonderland" };
 
 for (const key in person) {
     console.log(key + ": " + person[key]);
+
+
+};
+
+
+// compinateion array and object
+const persons = [
+    { name: "Ahmed", age: 25, city: "Mogadishu" },
+    { name: "Ikhlaas", age: 17, city: "Somalia" },
+];
+
+for (const person of persons) {
+    for (const key in person) {
+        console.log(key + ": " + person[key]);
+    }
 }
