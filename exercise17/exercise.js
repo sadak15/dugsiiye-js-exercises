ab7ff308f77982d10815eb62595476d56a807a09
sadak15 <sadakc15@gmail.com>
2026-01-29 00:03:01 +0300

    exercise 17

---
+++ exercise17/exercise.js
@@ -0,0 +1,12 @@
+
+Temprature = 26;
+
+if(Temprature < 0){
+    console.log("Very Cold");
+}else if(Temprature < 15){
+    console.log("Cold");
+}else if(Temprature < 25){
+    console.log("Warm");
+}else{
+    console.log("Hot")
+}
