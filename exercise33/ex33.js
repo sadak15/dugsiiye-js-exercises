04e20689881da1569982fce78ed3d55baf815e7a
sadak15 <sadakc15@gmail.com>
2026-03-06 17:51:07 +0300

    exercise 33

---
+++ exercise33/ex33.js
@@ -0,0 +1,15 @@
+
+const name = document.querySelector('#name');
+
+const text = document.querySelector('.text');
+
+
+function changeName(){
+    name.textContent = "Sadak Ali"
+}
+
+
+function changeText() {
+    text.innerHTML = 'hi welcome to my new info <Strong>subscribe my page </strong>'
+
+}
