18a94f1feddf6eba5b4d54e50c41d37698e64eb9
sadak15 <sadakc15@gmail.com>
2026-03-06 17:26:35 +0300

    exercise 32

---
+++ exercise32/ex32.js
@@ -0,0 +1,17 @@
+
+//by id
+const name = document.querySelector('#name');
+
+console.log(name);
+
+// by clas name
+const text = document.querySelectorAll('.text');
+
+console.log(text);
+
+
+//by tag name
+
+const tag = document.querySelector('h1');
+
+console.log(tag);
